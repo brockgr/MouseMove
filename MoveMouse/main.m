a47ebfa2b873c013a95d7c58bcc9057d2122b9d2
Gavin Brock <gavin@brock-family.org>
2013-11-04 13:55:32 +0100

    Initial Commit

---
+++ MoveMouse/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  MoveMouse
+//
+//  Created by Gavin Brock on 11/4/13.
+//  Copyright (c) 2013 Gavin Brock. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
